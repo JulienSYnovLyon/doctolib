bfdc074696973f8a9c4e430ea86afe846d594248
JulienSYnovLyon <42091688+JulienSYnovLyon@users.noreply.github.com>
2023-03-06 10:28:37 +0100

    manifest update
    manifest update

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,36 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [react(), VitePWA({
     manifest: {
-      name: 'Doctolib-App',
-      short_name: 'Docto',
-      icons: [
-        {
-          src: '/assets/icone-doctolib192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/assets/icone-doctolib512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-    },
+      "theme_color": "#4835f6",
+      "background_color": "#ffffff",
+      "display": "browser",
+      "scope": "/",
+      "start_url": "/",
+      "name": "doctolib project",
+      "short_name": "doctolib",
+      "description": "projet doctolib",
+      "icons": [
+          {
+              "src": "/icon-192x192.png",
+              "sizes": "192x192",
+              "type": "image/png"
+          },
+          {
+              "src": "/icon-256x256.png",
+              "sizes": "256x256",
+              "type": "image/png"
+          },
+          {
+              "src": "/icon-384x384.png",
+              "sizes": "384x384",
+              "type": "image/png"
+          },
+          {
+              "src": "/icon-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png"
+          }
+      ]
+  },
   })],
 });
