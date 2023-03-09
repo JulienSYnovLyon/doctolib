56d5835c95019f2bceddb7af24d5e7157e8602c4
JulienSYnovLyon <42091688+JulienSYnovLyon@users.noreply.github.com>
2023-03-09 14:53:35 +0100

    liste de rdv
    liste de rdv avec faux nom car par réussi a prendre les vrai

--- src/routes/Rdv.jsx
+++ src/routes/Rdv.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect  } from 'react';
 import logo from '../assets/icone-doctolib192x192.png';
 import { getAuth} from "firebase/auth"
-import { collection, getDocs , query, where} from "firebase/firestore";
+import { collection, getDocs , query, where , doc} from "firebase/firestore";
 import db from '../firebase';
 import './RDV.css';
 
@@ -18,14 +18,35 @@ function RDVPage() {
         });
 
         const fetchrdv = async (user) => {
-
             const querySnapshot = await getDocs(collection(db, "rdv"), where("patient", "==", user.uid));
-            const newData = querySnapshot.docs.map((doc) => {
-                const datetime = new Date(doc.data().date);
-                return {date: datetime, medecin: doc.data().medecin};
-            });
+            const newData = [];
+            for (const doc1 of querySnapshot.docs) {
+              const datetime = new Date(doc1.data().date);
+              const medecinId = doc1.data().medecin;
+              let nom;
+              let prenom ; 
+              if(medecinId == "dT4bWeMZJv1juTeLDD9a")
+              {
+                    nom  = "dupont";
+                    prenom = "jean"; 
+              }
+              else if (medecinId == "fESjTkdQj4JnTgO9wKqh")
+              {
+                    nom  = "durieux";
+                    prenom = "daniel"; 
+              }
+              else
+              {
+                    nom  = "un nom";
+                    prenom = "un preom"; 
+              }
+              //const medecinDoc = await getDocs(doc(db, "medecins", medecinId));
+              //const medecinData = medecinDoc.data();
+              //newData.push({ date: datetime, medecin: medecinData.nom + " " + medecinData.prenom });
+              newData.push({ date: datetime, medecin: nom + " " + prenom });
+            }
             setRdvList(newData);
-        };
+          };
 
     }, []);
   
--- vite.config.js
+++ vite.config.js
@@ -36,5 +36,8 @@ export default defineConfig({
           }
       ]
   },
+  devOptions: {
+    enabled: true
+  }
   })],
 });
