90dd9b979500448e40ab31e3726329382f0791a5
JulienSYnovLyon <42091688+JulienSYnovLyon@users.noreply.github.com>
2023-03-09 15:01:06 +0100

    update conf

--- vite.config.js
+++ vite.config.js
@@ -3,41 +3,40 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    manifest: {
-      "theme_color": "#4835f6",
-      "background_color": "#ffffff",
-      "display": "browser",
-      "scope": "/",
-      "start_url": "/",
-      "name": "doctolib project",
-      "short_name": "doctolib",
-      "description": "projet doctolib",
-      "icons": [
+  plugins: [
+    react(), 
+    VitePWA({
+        "scope": "/",
+        "start_url": "/",
+        "name": "doctolib project",
+        "short_name": "doctolib",
+        "description": "projet doctolib",
+        "icons": [
           {
-              "src": "/icon-192x192.png",
-              "sizes": "192x192",
-              "type": "image/png"
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
           {
-              "src": "/icon-256x256.png",
-              "sizes": "256x256",
-              "type": "image/png"
+            "src": "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
           },
           {
-              "src": "/icon-384x384.png",
-              "sizes": "384x384",
-              "type": "image/png"
+            "src": "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
           },
           {
-              "src": "/icon-512x512.png",
-              "sizes": "512x512",
-              "type": "image/png"
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           }
-      ]
-  },
+        ]
+      }
+    })
+  ],
   devOptions: {
     enabled: true
   }
-  })],
 });
